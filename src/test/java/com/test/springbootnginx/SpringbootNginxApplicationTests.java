6977c77b96a4694450b8a3b817aa59edef280bb0
Bruno Queiroz <bruno.queiroz@liferay.com>
2023-12-04 15:29:29 -0300

    Initial commit

---
+++ src/test/java/com/test/springbootnginx/SpringbootNginxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.springbootnginx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootNginxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
